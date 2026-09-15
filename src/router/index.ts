fc7f2f86d21c7cfedcc128f7af9c1ab745bba2b8
WALKER <woodpk@sesc.bz>
2026-09-15 15:13:22 +0900

    GitHub F5対策 ルートに#

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,14 +5,15 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import index from '@/pages/index.vue'
 import horizontal from '@/pages/horizontal.vue';
 import virtical from '@/pages/virtical.vue';
 import list from '@/pages/list.vue';
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    //history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL), 
     routes: [
         { path: '/', component: index, meta: { title: 'カタカムナ・うずまき' } },
         { path: '/horizontal', component: horizontal, meta: { title: 'カタカムナ・横書き' } },
